5010d4b2eb7f14f49e5d3ed3360774610206181d
Lennart Klein <lennart.klein@eo.nl>
2021-05-05 16:31:00 +0200

    install NuxtJS

---
+++ nuxt.config.js
@@ -0,0 +1,44 @@
+export default {
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint: process.env.BACKEND_URL || 'http://localhost:1337/graphql'
+      }
+    }
+  },
+  axios: {},
+  build: {
+  },
+  buildModules: [
+    '@nuxtjs/eslint-module'
+  ],
+  css: [
+  ],
+  components: true,
+  env: {
+    strapiBaseUri: process.env.API_URL || 'http://localhost:1337'
+  },
+  head: {
+    title: 'koeching',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    '@nuxtjs/apollo',
+  ],
+  plugins: [
+  ],
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        koeching
+      </h1>
+      <a
+        v-for="colleague in colleagues"
+        :key="colleague.name"
+        :href="colleague.website"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {{ colleague.name }}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import colleaguesQuery from '~/apollo/queries/colleague/colleagues'
+
+export default {
+  data () {
+    return {
+      colleagues: []
+    }
+  },
+  apollo: {
+    colleagues: {
+      prefetch: true,
+      query: colleaguesQuery
+    }
+  }
+}
+</script>
