ff5177bbb69dcbc808b9559ca1e42f6ae8e818ca
Lennart Klein <lennart.klein@eo.nl>
2023-01-01 17:19:19 +0100

    add animals illustration to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,23 @@
 <template>
   <PageTitle class="sr-only">(Kinder)coaching op de boerderij</PageTitle>
-  <Hero image="borreveld">
+  <Hero image="borreveld" blur-image>
     <img
       class="mx-auto mt-3 w-64"
       src="~/assets/svg/logo.svg"
       alt="Logo van Koeching"
     />
+    <img
+      class="-mx-8 mt-auto max-w-md"
+      src="~/assets/svg/animals-inviting-hug.svg"
+      alt="Dieren van Koeching"
+    />
   </Hero>
   <SpeechBubble color="white" class="mx-3 -mt-12">
     Welkom op de site van Koeching!<br />Leuk dat je er bent.
   </SpeechBubble>
+  <p class="my-12 mx-3 italic">Todo: introductievideos</p>
+  <p class="my-12 mx-3 italic">Todo: agenda / kalender</p>
+  <p class="my-12 mx-3 italic">
+    Todo: footer met linkjes naar voorwaarden, tarieven, andere coaches, etc
+  </p>
 </template>
