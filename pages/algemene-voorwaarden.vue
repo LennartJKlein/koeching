395887c54df9aa1c04ece323d6e543be50a26ac3
Lennart Klein <lennart.klein@eo.nl>
2023-04-14 20:34:46 +0200

    update styling of legal texts

--- pages/algemene-voorwaarden.vue
+++ pages/algemene-voorwaarden.vue
@@ -20,17 +20,16 @@ const { classes: contentClasses } = useContentStyles()
     <div
       v-if="terms"
       :class="`
-          -mx-4
-          max-w-4xl
-          bg-[url(~/assets/svg/paper-top.svg),url(~/assets/svg/paper-middle.svg),url(~/assets/svg/paper-bottom.svg)]
-          bg-[length:100%_auto,100%_calc(100%-52vw),100%_auto]
-          bg-[position:center_top,center_38vw,center_bottom]
-          bg-no-repeat
-          px-[10vw]
-          pt-[9vw] pb-[8vw] drop-shadow-2xl
-          md:mx-auto
-          md:bg-[length:100%_auto,100%_calc(100%-380px),100%_auto] md:bg-[position:center_top,center_280px,center_bottom]
-          lg:px-20 lg:pt-20 lg:pb-10`"
+        -mx-4 max-w-4xl
+        bg-[url(~/assets/svg/paper-top.svg),url(~/assets/svg/paper-middle.svg),url(~/assets/svg/paper-bottom.svg)]
+        bg-[length:100%_auto,100%_calc(100%-52vw),100%_auto]
+        bg-[position:center_top,center_38vw,center_bottom]
+        bg-no-repeat px-[10vw] pt-[9vw] pb-[8vw] drop-shadow-2xl
+        md:mx-auto
+        md:bg-[length:100%_auto,100%_calc(100%-380px),100%_auto]
+        md:bg-[position:center_top,center_280px,center_bottom]
+        lg:px-20 lg:pt-20 lg:pb-10
+      `"
     >
       <div
         :class="contentClasses"
