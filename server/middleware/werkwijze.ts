2a6da94518e127a75aeba964949f5d1b8d0a4bec
Lennart Klein <lennart.klein@eo.nl>
2023-09-18 19:06:10 +0200

    fix redirect of werkwijze

--- server/middleware/werkwijze.ts
+++ server/middleware/werkwijze.ts
@@ -3,6 +3,6 @@ export default defineEventHandler((event) => {
   if (url.pathname === '/__nuxt_error') return
 
   if (url.pathname == '/werkwijze') {
-    sendRedirect(event, '/aanbod/werkwijze-en-procedure')
+    sendRedirect(event, '/aanbod/trajecten/werkwijze-en-procedure')
   }
 })
